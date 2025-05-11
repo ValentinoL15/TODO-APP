b20cd88b7404e1ec71506b9f3796b42b00c1d01d
ValentinoL15 <pichilongo1@gmail.com>
2025-05-11 19:46:46 -0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './publish/login/login.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', loadComponent: () => import('./private/home/home.component').then(m => m.HomeComponent) },
   { path: 'task-detail/:id', loadComponent: () => import('./private/task-components/task-detail/task-detail.component').then(m => m.TaskDetailComponent) }, 
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: '**', redirectTo: '' }
 ];
